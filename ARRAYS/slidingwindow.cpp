d08bbcd4162a2c9833032165034668f77d9e6ef1
kipkogeic <kipronomeshack36@gmail.com>
2025-10-07 22:52:24 +0300

    Update the whole template for the sliding window technique

--- ARRAYS/slidingwindow.cpp
+++ ARRAYS/slidingwindow.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int maxSumSubarray(vector<int> &arr, int k) {
+    int n = arr.size();
+    int window_sum = 0, max_sum = 0;
+    // Compute sum of first window
+    for (int i = 0; i < k; i++) {
+        window_sum += arr[i];
+    }
+    max_sum = window_sum;
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        window_sum += arr[i] - arr[i - k]; // Add next element, remove first in window
+        max_sum = max(max_sum, window_sum);
+    }
+
+    return max_sum;
+}
 
 int main() {
     //the sliding window technique
+    //fixed
+    vector<int> arr = {2, 1, 5, 1, 3, 2};
+    int k = 3;
+    cout << "Maximum sum of subarray of size " << k << " is " << maxSumSubarray(arr, k);
+    return 0;
+
     return 0;
 }
